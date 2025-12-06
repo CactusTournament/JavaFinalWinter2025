3be86882bd8329507367e72e06d652f7edde8604
Magret <belloabiodun17@gmail.com>
2025-12-06 00:56:51 -0330

    Added super class user

---
+++ User.java
@@ -0,0 +1,154 @@
+package JavaFinalWinter2025;
+
+/**
+ * User class representing a user in the system.
+ * Fields correspond to the Users table in the database.
+ * 
+ * Fields:
+ * - userId: Unique identifier for the user
+ * - userName: Name of the user
+ * - userAddress: Address of the user
+ * - userPhoneNumber: Phone number of the user
+ * - userRole: Role of the user ('Admin', 'Trainer', 'Member')
+ * - passwordHash: Hashed password for security
+ * - email: Email address of the user
+ * 
+ * @author: Abiodun Magret Oyedele
+ * Date: 2025-12-06
+ */
+public class User {
+    private int userId;
+    private String userName;
+    private String userAddress;
+    private String userPhoneNumber;
+    private String userRole; // 'Admin', 'Trainer', 'Member'
+    private String passwordHash;
+    private String email;
+
+    /**
+     * Getter for userId.
+     * @return the userId
+     */
+    public int getUserId() {
+        return userId;
+    }
+
+    /**
+     * Setter for userId.
+     * @param userId the userId to set
+     */
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * Getter for userName.
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * Setter for userName.
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * Getter for userAddress.
+     * @return the userAddress
+     */
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    /**
+     * Setter for userAddress.
+     * @param userAddress the userAddress to set
+     */
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    /**
+     * Getter for userPhoneNumber.
+     * @return the userPhoneNumber
+     */
+    public String getUserPhoneNumber() {
+        return userPhoneNumber;
+    }
+
+    /**
+     * Setter for userPhoneNumber.
+     * @param userPhoneNumber the userPhoneNumber to set
+     */
+    public void setUserPhoneNumber(String userPhoneNumber) {
+        this.userPhoneNumber = userPhoneNumber;
+    }
+
+    /**
+     * Getter for userRole.
+     * @return the userRole
+     */
+    public String getUserRole() {
+        return userRole;
+    }
+
+    /**
+     * Setter for userRole.
+     * @param userRole the userRole to set
+     */
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    /**
+     * Getter for passwordHash.
+     * @return the passwordHash
+     */
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    /**
+     * Setter for passwordHash.
+     * @param passwordHash the passwordHash to set
+     */
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    /**
+     * Getter for email.
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Setter for email.
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Override toString method for better representation.
+     */
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", userAddress='" + userAddress + '\'' +
+                ", userPhoneNumber='" + userPhoneNumber + '\'' +
+                ", userRole='" + userRole + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
