8bf070b1a191c81e86fb0b2f1e11b5fc5a3223cb
Magret <belloabiodun17@gmail.com>
2025-12-09 18:01:25 -0330

    Added JavaDocs to the test files

--- TestDAO.java
+++ TestDAO.java
@@ -2,10 +2,19 @@ package JavaFinalWinter2025;
 
 import JavaFinalWinter2025.dao.UserDAO;
 import JavaFinalWinter2025.src.User;
-
 import java.util.List;
 
+/**
+ * TestDAO class to test UserDAO functionality.
+ * 
+ * @author: Brandon Maloney
+ */
 public class TestDAO {
+    /**
+     * Main method to run UserDAO test.
+     * 
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
         UserDAO dao = new UserDAO();
 
